234870495132d85d461bfc4303d6c7a60ad500db
ChauVanLoc <19130121@st.hcmuaf.edu.vn>
2023-06-26 17:29:16 +0700

    update ui

--- QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/views/History.java
+++ QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/views/History.java
@@ -13,111 +13,147 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.LineBorder;
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import javax.swing.JTabbedPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+import models.ProductObserver;
+
+import java.awt.Component;
 
 public class History extends JPanel {
 	private JTable tableHistory;
-	private JTable table;
+	private JTabbedPane tabbedPane;
 
 	public History() {
+		setBackground(SystemColor.window);
 		setBounds(0, 0, 1228, 619);
 		setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Tổng chi tiêu");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel.setBounds(82, 22, 114, 30);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblNewLabel.setBounds(82, 10, 114, 30);
 		add(lblNewLabel);
-		
+
 		JLabel lblTngnHng = new JLabel("Tổng đơn hàng");
-		lblTngnHng.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblTngnHng.setBounds(308, 22, 140, 30);
+		lblTngnHng.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblTngnHng.setBounds(308, 10, 140, 30);
 		add(lblTngnHng);
-		
+
 		JLabel lblnHy = new JLabel("Đã hủy");
-		lblnHy.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblnHy.setBounds(879, 62, 108, 30);
+		lblnHy.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblnHy.setBounds(879, 50, 108, 30);
 		add(lblnHy);
-		
+
 		JLabel lblChVnChuyn = new JLabel("Chờ vận chuyển");
-		lblChVnChuyn.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblChVnChuyn.setBounds(570, 62, 140, 30);
+		lblChVnChuyn.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblChVnChuyn.setBounds(570, 50, 140, 30);
 		add(lblChVnChuyn);
-		
+
 		JLabel lblThnhCng = new JLabel("Thành công");
-		lblThnhCng.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblThnhCng.setBounds(570, 22, 108, 30);
+		lblThnhCng.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblThnhCng.setBounds(570, 10, 108, 30);
 		add(lblThnhCng);
-		
+
 		JLabel lblChThanhTon = new JLabel("Chờ thanh toán");
-		lblChThanhTon.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblChThanhTon.setBounds(879, 22, 140, 30);
+		lblChThanhTon.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblChThanhTon.setBounds(879, 10, 140, 30);
 		add(lblChThanhTon);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("10$");
+		lblNewLabel_1.setForeground(new Color(153, 0, 0));
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1.setBounds(82, 62, 182, 30);
+		lblNewLabel_1.setBounds(82, 50, 182, 30);
 		add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_1_1 = new JLabel("10$");
+		lblNewLabel_1_1.setForeground(new Color(153, 0, 0));
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_1.setBounds(308, 62, 182, 30);
+		lblNewLabel_1_1.setBounds(308, 50, 182, 30);
 		add(lblNewLabel_1_1);
-		
+
 		JLabel lblNewLabel_1_2 = new JLabel("100");
+		lblNewLabel_1_2.setForeground(SystemColor.desktop);
 		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_2.setBounds(729, 22, 108, 30);
+		lblNewLabel_1_2.setBounds(740, 10, 108, 30);
 		add(lblNewLabel_1_2);
-		
+
 		JLabel lblNewLabel_1_2_1 = new JLabel("100");
 		lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_2_1.setBounds(729, 62, 108, 30);
+		lblNewLabel_1_2_1.setBounds(740, 50, 108, 30);
 		add(lblNewLabel_1_2_1);
-		
+
 		JLabel lblNewLabel_1_2_2 = new JLabel("100");
+		lblNewLabel_1_2_2.setForeground(new Color(204, 102, 0));
 		lblNewLabel_1_2_2.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_2_2.setBounds(1028, 22, 108, 30);
+		lblNewLabel_1_2_2.setBounds(1047, 10, 108, 30);
 		add(lblNewLabel_1_2_2);
-		
+
 		JLabel lblNewLabel_1_2_2_1 = new JLabel("100");
 		lblNewLabel_1_2_2_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_2_2_1.setBounds(1028, 62, 108, 30);
+		lblNewLabel_1_2_2_1.setBounds(1047, 50, 108, 30);
 		add(lblNewLabel_1_2_2_1);
-		
-		JButton btnNewButton = new JButton("Tất cả");
-		btnNewButton.setFocusPainted(false);
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnNewButton.setBounds(82, 114, 114, 30);
-		add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Chờ vận chuyển");
-		btnNewButton_1.setFocusPainted(false);
-		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnNewButton_1.setBounds(194, 114, 160, 30);
-		add(btnNewButton_1);
-		
-		JButton btnThnhCng = new JButton("Thành công");
-		btnThnhCng.setFocusPainted(false);
-		btnThnhCng.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnThnhCng.setBounds(348, 114, 136, 30);
-		add(btnThnhCng);
-		
-		JButton btnHy = new JButton("Đã hủy");
-		btnHy.setFocusPainted(false);
-		btnHy.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnHy.setBounds(482, 114, 114, 30);
-		add(btnHy);
-		
-		String[][] data = {
-				{ "Kundan Kumar Jha", "4031", "CSE" },
-				{ "Anand Jha", "6014", "IT" }
-		};
-		// Column Names
-		String[] columnNames = { "Name", "Roll Number", "Department" };
-		table = new JTable(data, columnNames);
-		table.setBounds(82, 341, 1054, -169);
-		add(table);
-		
 
-		
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setFont(new Font("Tahoma", Font.BOLD, 15));
+		tabbedPane.setAlignmentY(Component.TOP_ALIGNMENT);
+		tabbedPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+		tabbedPane.setBackground(new Color(153, 204, 204));
+		tabbedPane.setBorder(null);
+		tabbedPane.setBounds(82, 90, 1059, 507);
+		add(tabbedPane);
+
+		JPanel panel_total = new JPanel();
+		panel_total.setBackground(SystemColor.window);
+		panel_total.setBorder(null);
+		panel_total.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tabbedPane.add("Total", panel_total);
+		tabbedPane.setBackgroundAt(0, new Color(153, 204, 204));
+		panel_total.setLayout(null);
+
+		JPanel panel_success = new JPanel();
+		panel_success.setBackground(SystemColor.window);
+		panel_success.setBorder(null);
+		panel_success.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tabbedPane.add("Success", panel_success);
+
+		JPanel panel_delivering = new JPanel();
+		panel_delivering.setBackground(SystemColor.window);
+		panel_delivering.setBorder(null);
+		panel_delivering.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tabbedPane.add("Delivering", panel_delivering);
+
+		JPanel panel_cancel = new JPanel();
+		panel_cancel.setBackground(SystemColor.window);
+		panel_cancel.setBorder(null);
+		panel_cancel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tabbedPane.add("Cancel", panel_cancel);
+
+		JPanel panel_waiting_pay = new JPanel();
+		panel_waiting_pay.setAlignmentY(Component.TOP_ALIGNMENT);
+		panel_waiting_pay.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panel_waiting_pay.setBackground(SystemColor.window);
+		panel_waiting_pay.setBorder(null);
+		panel_waiting_pay.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tabbedPane.add("Pay", panel_waiting_pay);
+		panel_waiting_pay.setLayout(null);
+
+		String[][] data = { { "Kundan Kumar Jha", "4031", "CSE" }, { "Anand Jha", "6014", "IT" } };
+		String[] columnNames = { "Name", "Roll Number", "Department" };
+
 	}
+
+	public JScrollPane createScrollPane(JPanel panel, List<Object> datas, List<String> columnTitles) {
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+
+		JTable table_1 = new JTable();
+		scrollPane.setViewportView(table_1);
+
+		return scrollPane;
+	}
+
 }
--- QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/views/Profile.java
+++ QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/views/Profile.java
@@ -1,19 +1,22 @@
 package views;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Insets;
-import java.awt.RenderingHints;
-import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -24,6 +27,8 @@ import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Profile extends JPanel {
 	private JTextField textFieldFullName;
@@ -33,94 +38,39 @@ public class Profile extends JPanel {
 	private JPasswordField passwordField;
 	private JPasswordField passwordField_1;
 	private JPasswordField passwordField_2;
-	private JTextField textFieldCreditCart;
+	private JPasswordField passwordField_3;
+	private JLabel lb_avatar;
+	private JButton btnChangeImage;
+	private JRadioButton rdbtnFemale;
+	private JRadioButton rdbtnMale;
+	private JRadioButton rdbtnOtherGender;
 
 	/**
 	 * Create the panel.
 	 */
 	public Profile() {
-		setBounds(0, 0, 1184, 570);
+		setBackground(SystemColor.window);
+		setBounds(0, 0, 1228, 619);
 		setLayout(null);
-
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 305, 548);
-		add(panel);
-		panel.setLayout(null);
-
-		JLabel imgAvatar = new JLabel() {
-			@Override
-			protected void paintComponent(Graphics g) {
-				if (g instanceof Graphics2D) {
-					Graphics2D g2d = (Graphics2D) g;
-					g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-					Shape shape = new Ellipse2D.Double(0, 0, getWidth(), getHeight());
-					g2d.clip(shape);
-				}
-				super.paintComponent(g);
-			}
-		};
 		ImageIcon juice = new ImageIcon("../FastFoodStoreManager/img/bird.jpg");
 		Image imageJuice = juice.getImage();
 		Image resizeImageJuice = imageJuice.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
 		ImageIcon juiceImg = new ImageIcon(resizeImageJuice);
-		imgAvatar.setIcon(juiceImg);
-		imgAvatar.setBounds(50, 50, 200, 199);
-		panel.add(imgAvatar);
-
-		JLabel lblNewLabel = new JLabel("PROFILE");
-		lblNewLabel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblNewLabel.setBounds(0, 0, 305, 50);
-		panel.add(lblNewLabel);
-
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(0, 250, 305, 298);
-		panel.add(panel_2);
-		panel_2.setLayout(null);
-
-		JLabel lblNewLabel_1 = new JLabel("TRẦN TÚ NGUYỆT");
-		lblNewLabel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 17));
-		lblNewLabel_1.setBounds(0, 0, 305, 40);
-		panel_2.add(lblNewLabel_1);
-
-		JButton btLogOut = new JButton("Log Out");
-		btLogOut.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btLogOut.setBounds(0, 158, 300, 50);
-		panel_2.add(btLogOut);
-
-		JButton btDeleteAccount = new JButton("Delete Account");
-		btDeleteAccount.setMargin(new Insets(0, 0, 0, 0));
-		btDeleteAccount.setForeground(Color.LIGHT_GRAY);
-		btDeleteAccount.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btDeleteAccount.setBounds(0, 248, 300, 50);
-		panel_2.add(btDeleteAccount);
-
-		JPanel panel_6 = new JPanel();
-		panel_6.setBounds(0, 42, 305, 105);
-		panel_2.add(panel_6);
-		panel_6.setLayout(null);
-
-		JLabel lblRankCustomer = new JLabel("Gold Customer");
-		lblRankCustomer.setBounds(20, 31, 144, 50);
-		lblRankCustomer.setFont(new Font("MingLiU-ExtB", Font.BOLD, 17));
-		panel_6.add(lblRankCustomer);
-
-		JLabel imgRankCustomer = new JLabel("img rank");
-		imgRankCustomer.setBounds(174, 0, 100, 100);
-		panel_6.add(imgRankCustomer);
 
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(325, 11, 849, 548);
+		panel_1.setBackground(SystemColor.textHighlightText);
+		panel_1.setBounds(0, 0, 1228, 619);
 		add(panel_1);
 		panel_1.setLayout(null);
 
 		JPanel panel_3 = new JPanel();
+		panel_3.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		panel_3.setAlignmentY(Component.TOP_ALIGNMENT);
+		panel_3.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panel_3.setBackground(SystemColor.window);
 		panel_3.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_3.setBounds(0, 0, 450, 548);
+		panel_3.setBounds(0, 0, 718, 619);
 		panel_1.add(panel_3);
 		panel_3.setLayout(null);
 
@@ -128,54 +78,54 @@ public class Profile extends JPanel {
 		lblNewLabel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewLabel_2.setBounds(0, 0, 450, 50);
+		lblNewLabel_2.setBounds(0, 0, 718, 50);
 		panel_3.add(lblNewLabel_2);
 
 		JLabel lblFullName = new JLabel("Name");
 		lblFullName.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName.setBounds(10, 75, 150, 30);
+		lblFullName.setBounds(253, 70, 150, 30);
 		panel_3.add(lblFullName);
 
 		textFieldFullName = new JTextField();
 		textFieldFullName.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		textFieldFullName.setText("Trần Tú Nguyệt");
-		textFieldFullName.setBounds(170, 75, 270, 30);
+		textFieldFullName.setBounds(413, 70, 270, 30);
 		panel_3.add(textFieldFullName);
 		textFieldFullName.setColumns(10);
 
 		JLabel lblFullName_1 = new JLabel("PhoneNumber");
 		lblFullName_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_1.setBounds(10, 260, 150, 30);
+		lblFullName_1.setBounds(253, 264, 150, 30);
 		panel_3.add(lblFullName_1);
 
-		JLabel lblFullName_2 = new JLabel("Citizen identification");
+		JLabel lblFullName_2 = new JLabel("CCCD");
 		lblFullName_2.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_2.setBounds(10, 300, 160, 30);
+		lblFullName_2.setBounds(253, 314, 160, 30);
 		panel_3.add(lblFullName_2);
 
 		JLabel lblFullName_3 = new JLabel("Address");
 		lblFullName_3.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_3.setBounds(10, 340, 150, 30);
+		lblFullName_3.setBounds(253, 369, 150, 30);
 		panel_3.add(lblFullName_3);
 
 		txtxxx = new JTextField();
 		txtxxx.setText("091494XXX");
 		txtxxx.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		txtxxx.setColumns(10);
-		txtxxx.setBounds(170, 260, 270, 30);
+		txtxxx.setBounds(413, 264, 270, 30);
 		panel_3.add(txtxxx);
 
 		textField_1 = new JTextField();
 		textField_1.setText("031 002 034 7895");
 		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		textField_1.setColumns(10);
-		textField_1.setBounds(170, 300, 270, 30);
+		textField_1.setBounds(413, 314, 270, 30);
 		panel_3.add(textField_1);
 
 		JScrollPane scrollPaneAddress = new JScrollPane();
 		scrollPaneAddress.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPaneAddress.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPaneAddress.setBounds(170, 340, 270, 152);
+		scrollPaneAddress.setBounds(413, 369, 270, 152);
 		panel_3.add(scrollPaneAddress);
 
 		JTextArea textArea = new JTextArea();
@@ -183,27 +133,43 @@ public class Profile extends JPanel {
 		textArea.setLineWrap(true);
 		scrollPaneAddress.setViewportView(textArea);
 
-		JRadioButton rdbtnFemale = new JRadioButton("Female");
-		rdbtnFemale.setBounds(170, 116, 70, 23);
+		rdbtnFemale = new JRadioButton("Female");
+		rdbtnFemale.setFocusPainted(false);
+		rdbtnFemale.setBackground(SystemColor.window);
+		rdbtnFemale.setSelected(true);
+		rdbtnFemale.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		rdbtnFemale.setBounds(413, 118, 86, 23);
 		panel_3.add(rdbtnFemale);
 
 		JLabel lblFullName_1_1 = new JLabel("Gender");
 		lblFullName_1_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_1_1.setBounds(10, 116, 150, 30);
+		lblFullName_1_1.setBounds(253, 118, 150, 30);
 		panel_3.add(lblFullName_1_1);
 
 		JLabel lblBirthday = new JLabel("Day Of Birthday");
 		lblBirthday.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblBirthday.setBounds(10, 159, 150, 30);
+		lblBirthday.setBounds(253, 158, 150, 30);
 		panel_3.add(lblBirthday);
 
-		JRadioButton rdbtnMale = new JRadioButton("Male");
-		rdbtnMale.setBounds(242, 116, 70, 23);
+		rdbtnMale = new JRadioButton("Male");
+		rdbtnMale.setFocusPainted(false);
+		rdbtnMale.setBackground(SystemColor.window);
+		rdbtnMale.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		rdbtnMale.setBounds(501, 118, 70, 23);
 		panel_3.add(rdbtnMale);
 
-		JRadioButton rdbtnOtherGender = new JRadioButton("Other");
-		rdbtnOtherGender.setBounds(314, 116, 70, 23);
+		rdbtnOtherGender = new JRadioButton("Other");
+		rdbtnOtherGender.setFocusPainted(false);
+		rdbtnOtherGender.setBackground(SystemColor.window);
+		rdbtnOtherGender.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		rdbtnOtherGender.setBounds(573, 118, 80, 23);
 		panel_3.add(rdbtnOtherGender);
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnFemale);
+		group.add(rdbtnMale);
+		group.add(rdbtnOtherGender);
+		
 
 		// Tạo danh sách ngày, tháng, năm
 		DefaultListModel<String> dayListModel = new DefaultListModel<>();
@@ -222,70 +188,58 @@ public class Profile extends JPanel {
 		}
 
 		JButton btnSave = new JButton("SAVE");
+		btnSave.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnSave.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnSave.setBounds(290, 504, 150, 30);
+		btnSave.setBounds(533, 556, 150, 30);
 		panel_3.add(btnSave);
 
 		JLabel lblBirthday_1 = new JLabel("Email");
 		lblBirthday_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblBirthday_1.setBounds(10, 219, 150, 30);
+		lblBirthday_1.setBounds(253, 214, 150, 30);
 		panel_3.add(lblBirthday_1);
 
 		txtTrantunguyetgmailcom = new JTextField();
 		txtTrantunguyetgmailcom.setText("trantunguyet@gmail.com");
 		txtTrantunguyetgmailcom.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		txtTrantunguyetgmailcom.setColumns(10);
-		txtTrantunguyetgmailcom.setBounds(170, 219, 270, 30);
+		txtTrantunguyetgmailcom.setBounds(413, 214, 270, 30);
 		panel_3.add(txtTrantunguyetgmailcom);
 
 		JComboBox comboBoxDay = new JComboBox();
 		comboBoxDay.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		comboBoxDay.setBounds(170, 150, 70, 40);
+		comboBoxDay.setBounds(413, 159, 70, 30);
 		panel_3.add(comboBoxDay);
 
 		JComboBox comboBoxMonth = new JComboBox();
 		comboBoxMonth.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		comboBoxMonth.setBounds(242, 150, 70, 40);
+		comboBoxMonth.setBounds(501, 158, 70, 30);
 		panel_3.add(comboBoxMonth);
 
 		JComboBox comboBoxYear = new JComboBox();
 		comboBoxYear.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		comboBoxYear.setBounds(314, 150, 70, 40);
+		comboBoxYear.setBounds(593, 158, 70, 30);
 		panel_3.add(comboBoxYear);
 
-		JLabel lblNewLabel_2_1 = new JLabel("ACCOUNT");
-		lblNewLabel_2_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewLabel_2_1.setBounds(450, 0, 400, 50);
-		panel_1.add(lblNewLabel_2_1);
+		lb_avatar = new JLabel("New label");
+		lb_avatar.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lb_avatar.setBounds(31, 70, 174, 174);
+		panel_3.add(lb_avatar);
 
-		JLabel lblFullName_4 = new JLabel("Account");
-		lblFullName_4.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_4.setBounds(460, 50, 150, 30);
-		panel_1.add(lblFullName_4);
-
-		JLabel lblFullName_5 = new JLabel("Trần Tú Nguyệt");
-		lblFullName_5.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_5.setBounds(650, 50, 150, 30);
-		panel_1.add(lblFullName_5);
-
-		JLabel lblFullName_6 = new JLabel("Password");
-		lblFullName_6.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_6.setBounds(460, 90, 150, 30);
-		panel_1.add(lblFullName_6);
-
-		passwordField = new JPasswordField();
-		passwordField.setBounds(650, 90, 200, 30);
-		panel_1.add(passwordField);
-
-		JLabel lblFullName_6_1 = new JLabel("New Password");
-		lblFullName_6_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_6_1.setBounds(460, 130, 150, 30);
-		panel_1.add(lblFullName_6_1);
+		btnChangeImage = new JButton("Change Image");
+		btnChangeImage.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnChangeImage.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnChangeImage.setBounds(31, 282, 174, 30);
+		panel_3.add(btnChangeImage);
+		btnChangeImage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showFileChooser();
+			}
+		});
 
 		JPanel panel_4 = new JPanel();
-		panel_4.setBounds(450, 246, 400, 302);
+		panel_4.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_4.setBackground(SystemColor.window);
+		panel_4.setBounds(717, 294, 511, 325);
 		panel_1.add(panel_4);
 		panel_4.setLayout(null);
 
@@ -293,47 +247,100 @@ public class Profile extends JPanel {
 		lblNewLabel_2_1_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNewLabel_2_1_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblNewLabel_2_1_1.setBounds(0, 0, 400, 41);
+		lblNewLabel_2_1_1.setBounds(0, 0, 511, 41);
 		panel_4.add(lblNewLabel_2_1_1);
-
-		JLabel lblCard = new JLabel();
 		ImageIcon creditCard = new ImageIcon("../FastFoodStoreManager/img/creditCard.jpg");
 		Image imageCreditCard = creditCard.getImage();
 		Image resizeImageCreditCard = imageCreditCard.getScaledInstance(367, 181, Image.SCALE_SMOOTH);
 		ImageIcon creditCardImg = new ImageIcon(resizeImageCreditCard);
-		lblCard.setIcon(creditCardImg);
-		lblCard.setBounds(10, 43, 367, 181);
-		panel_4.add(lblCard);
+
+		JLabel lblFullName_6_2 = new JLabel("Amount");
+		lblFullName_6_2.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblFullName_6_2.setBounds(47, 73, 150, 30);
+		panel_4.add(lblFullName_6_2);
+
+		passwordField_3 = new JPasswordField();
+		passwordField_3.setBounds(246, 73, 200, 30);
+		panel_4.add(passwordField_3);
 
 		JButton btnSave_1_1 = new JButton("SAVE");
-		btnSave_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnSave_1_1.setBounds(240, 261, 150, 30);
+		btnSave_1_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnSave_1_1.setBounds(264, 170, 150, 30);
 		panel_4.add(btnSave_1_1);
+		btnSave_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 
-		textFieldCreditCart = new JTextField();
-		textFieldCreditCart.setText("091494XXX");
-		textFieldCreditCart.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		textFieldCreditCart.setColumns(10);
-		textFieldCreditCart.setBounds(10, 229, 255, 30);
-		panel_4.add(textFieldCreditCart);
+		JPanel panel = new JPanel();
+		panel.setBorder(new MatteBorder(1, 0, 0, 1, (Color) new Color(0, 0, 0)));
+		panel.setBackground(SystemColor.window);
+		panel.setBounds(717, 0, 511, 294);
+		panel_1.add(panel);
+		panel.setLayout(null);
 
-		JLabel lblFullName_6_1_1 = new JLabel("Rewrite new password");
-		lblFullName_6_1_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblFullName_6_1_1.setBounds(460, 170, 200, 30);
-		panel_1.add(lblFullName_6_1_1);
+		JLabel lblNewLabel_2_1 = new JLabel("PASSWORD");
+		lblNewLabel_2_1.setBounds(0, 0, 511, 50);
+		panel.add(lblNewLabel_2_1);
+		lblNewLabel_2_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+
+		JLabel lblFullName_6 = new JLabel("Password");
+		lblFullName_6.setBounds(44, 67, 150, 30);
+		panel.add(lblFullName_6);
+		lblFullName_6.setFont(new Font("Tahoma", Font.PLAIN, 17));
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(243, 67, 200, 30);
+		panel.add(passwordField);
 
 		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(650, 131, 200, 30);
-		panel_1.add(passwordField_1);
+		passwordField_1.setBounds(243, 122, 200, 30);
+		panel.add(passwordField_1);
+
+		JLabel lblFullName_6_1 = new JLabel("New Password");
+		lblFullName_6_1.setBounds(44, 121, 150, 30);
+		panel.add(lblFullName_6_1);
+		lblFullName_6_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
+
+		JLabel lblFullName_6_1_1 = new JLabel("Confirm New Password");
+		lblFullName_6_1_1.setBounds(44, 179, 200, 30);
+		panel.add(lblFullName_6_1_1);
+		lblFullName_6_1_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
 
 		passwordField_2 = new JPasswordField();
-		passwordField_2.setBounds(650, 170, 200, 30);
-		panel_1.add(passwordField_2);
+		passwordField_2.setBounds(243, 179, 200, 30);
+		panel.add(passwordField_2);
 
 		JButton btnSave_1 = new JButton("SAVE");
+		btnSave_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnSave_1.setBounds(293, 235, 150, 30);
+		panel.add(btnSave_1);
 		btnSave_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnSave_1.setBounds(699, 211, 150, 30);
-		panel_1.add(btnSave_1);
 
+	}
+
+	public void showFileChooser() {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(new FileNameExtensionFilter("Images", "jpg", "jpeg", "png", "gif"));
+
+		int result = fileChooser.showOpenDialog(this);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fileChooser.getSelectedFile();
+			if (selectedFile != null) {
+				try {
+					// Đọc file ảnh và lấy kích thước
+					Image image = ImageIO.read(selectedFile);
+					int width = image.getWidth(null);
+					int height = image.getHeight(null);
+
+					// Tạo ảnh mới chỉ lấy nửa trái
+					Image leftHalfImage = image.getScaledInstance(width / 2, height, Image.SCALE_SMOOTH);
+
+					ImageIcon icon = new ImageIcon(leftHalfImage);
+					lb_avatar.setIcon(Helper.getImageIconScale(selectedFile.getAbsolutePath(), 200, 200));
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
 	}
 }
