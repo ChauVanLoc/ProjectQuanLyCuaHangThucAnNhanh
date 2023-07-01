e4a95a231b849e2e555f9ec3a1b58886829764f5
trantunguyet1 <91519397+trantunguyet1@users.noreply.github.com>
2023-07-01 12:48:43 +0700

    Merge remote-tracking branch 'origin/main' into Nguyệt_Ngu_Ngơ

--- QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/controller/MainController.java
+++ QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/controller/MainController.java
@@ -22,7 +22,28 @@ public class MainController {
 	}
 
 	public void initCustomer() {
-		center.register(null, null, null, null);
+		center.register("Pham Huy Hoang", "0812838212", "huyhoang@gamil.com", "huyhoang");
+		center.register("To Thi Thao", "0942323123", "thaocute@gmail.com", "123thaocute");
+		center.register("Nguyen Van Teo", "0123456789", "teoem@hotmail.com", "teodeptrai");
+		center.register("Le Dau", "0987654321", "dau@gmail.com", "daubanh01");
+		center.register("To Hoai", "0234567819", "hoai@gmail.com", "hoainhatho");
+		center.register("To Huu", "0876543219", "huucodao@gmail.com", "huunhavan");
+		center.register("Ho Xuan Huong", "0765432198", "nuchuacakhia@gmail.com", "yolo1010");
+		center.register("Xuan Dieu", "0543219876", "dieudiudang@gmail.com", "1056821bc");
+		center.register("Lam Thi Hoai Thuong", "0432199765", "thuongnhoai@gmail.com", "o92831");
+		center.register("Ma Nhat Huu", "0123556689", "123hudaica@gmail.com", "7483jdkk");
+		center.register("Banh Thi My Dieu", "088774321", "dieuhienthuc@gmail.com", "diuhienlanh");
+		center.register("Tran Thanh Tu", "0987654312", "tranthanhtu@gmail.com", "tu123456");
+		center.register("Dac Ki", "0123466779", "hoyeu@gmail.com", "12kkj12");
+		center.register("Khuong Tu Nha", "012998874", "nhatiensinh@gmail.com", "phongkas");
+		center.register("Tan Hoang Nhi", "0453631345", "nhinhi@gmail.com", "7682d");
+		center.register("Uyen Nhi Lam", "0383946175", "uyennhi@gmail.com", "2jjfh");
+		center.register("Truong Uyen", "0378263781", "uyenquadeptrai@gmail.com", "timenhemiu");
+		center.register("Ti Menh", "0378882936", "menhdaiti@gmail.com", "vanmenhcuatalauyenca");
+		center.register("Cuu Van Hoa", "0874646473", "hoa@gmail.com", "73hduoicato");
+		center.register("Truong Y", "099983876", "yeuhoa@gmail.com", "yeuhoanhieulam");
+		center.register("Huong Ly Nguyet", "0356726374", "nguyetly@gmail.com", "hksa33");
+		
 	}
 
 	public void init() {
