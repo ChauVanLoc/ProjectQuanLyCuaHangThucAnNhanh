d1b4b317db353428cc0784f800c85a04d8f20332
trantunguyet1 <91519397+trantunguyet1@users.noreply.github.com>
2023-07-03 15:05:03 +0700

    them du lieu initEmployee va Customer

--- QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/controller/MainController.java
+++ QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/controller/MainController.java
@@ -1,18 +1,55 @@
 package controller;
 
+import java.util.Date;
+
+import constant.Rule;
 import models.Admin;
 import models.Center;
 import models.PersonObserver;
+import models.ProductObserver;
 import models.Subject;
+import models.product.beverage.CarbonateSoftDrink;
+import models.product.beverage.Juice;
+import models.product.beverage.Mixed;
+import models.product.beverage.Tea;
+import models.product.beverageDecorator.Cheese;
+import models.product.beverageDecorator.Pudding;
+import models.product.beverageDecorator.Sugar;
+import models.product.food.BurgerBeef;
+import models.product.food.BurgerCheese;
+import models.product.food.BurgerChicken;
+import models.product.food.BurgerFish;
+import models.product.food.BurgerShrimp;
+import models.product.food.FrenchFries;
+import models.product.food.FriedChicken;
+import models.product.food.Spagetti;
+import models.product.foodDecorator.Beef;
+import models.product.foodDecorator.Chicken;
+import models.product.foodDecorator.Shrimp;
 import views.MainView;
 
 public class MainController {
 	private Subject center;
 
 	public void initEmployee(Admin admin) {
-		admin.createEmployeecreateEmployee(null, null, null, null, null, null, null, null, null);
+		admin.createEmployeecreateEmployee( Rule.Cashier, "098785746778", "Tran Tu Nguyet", new Date(2001, 5, 10), new Date(2025, 5, 10), "Female", "Ho Chi Minh", "hoamuoigio2001@gmail.com", "0914945738");
+		admin.createEmployeecreateEmployee( Rule.KitchenStaff, "02232343232", "Tong Truc Phung Tran", new Date(2001, 10, 5), new Date(2025, 12, 24),"Famele", "Ho Chi Minh", "phungtran@gmail.com", "0888877773");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "098357483838", "Ngo Cu Van", new Date(2003, 9, 19), new Date(2024, 2, 25), "Male", "Binh Duong", "cuvandeptrai@gmail.com", "0533352635");
+		admin.createEmployeecreateEmployee(Rule.Cashier, "089736263726", "Chau Van Loc", new Date(2001, 5, 5), new Date(2023, 9, 21), "Male", "Tay Bac", "thaylocphuho@gmail.com", "0988873662");
+		admin.createEmployeecreateEmployee(Rule.Cashier, "093829484928", "Cao Mi Le", new Date(2000, 10, 10), new Date(2023, 9, 9), "Female", "Gia Lai", "camile@gmai.com", "0363637263");
+		admin.createEmployeecreateEmployee(Rule.KitchenStaff, "938293829389", "Le Sin", new Date(1999, 9, 20), new Date(2024, 12, 31), "Male", "Ninh Thuan", "lesin@gmail.com", "0988888979");
+		admin.createEmployeecreateEmployee(Rule.KitchenStaff, "098332322332", "Au Tra Son", new Date(1998, 12, 12), new Date(2025, 7, 12), "Male", "Ha Noi", "rongthan@gmail.com", "0993332221");
+		admin.createEmployeecreateEmployee(Rule.KitchenStaff, "092828182888", "Tran Trung Nhat", new Date(2002, 6, 30), new Date(2026, 8, 12), "Male", "An Giang", "nhat@gmail.com", "0347474878");
+		admin.createEmployeecreateEmployee(Rule.KitchenStaff, "022333233232", "Dao My Nhan", new Date(2002, 4, 24), new Date(2023, 12, 11), "Female", "Bac Ninh", "daomynhan@gmail.com", "0939393929");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "093938484575", "Phan Duy Manh", new Date(1997, 1, 12), new Date(2024, 12, 20), "Male", "Ho Chi Minh", "phaduymanh@gmail.com", "0737374747");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "099988777666", "Ha Thi Thu Uyen", new Date(2001, 7, 27), new Date(2023, 12, 24), "Female", "Ha Tinh", "uyen@gmail.com", "0999888991");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "033337774474", "Luu Manh Phu", new Date(2003, 6, 4), new Date(2024, 12, 5), "Male", "Da Nang", "phu@gmail.com", "0444555556");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "0666455463536", "Tu Nguyen Bao", new Date(1997, 3, 24), new Date(2025, 5, 30), "Male", "Ca Mau", "bao@gmail.com", "04455544543");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "0556666474689", "Luc Thanh Thanh", new Date(2001, 5, 28), new Date(2026, 6, 6), "Female", "Long An", "thanhthanh@gmail.com", "0345645673");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "088776655443", "Van Diep", new Date(2002, 2, 2), new Date(2026, 4, 6), "Male", "Son La", "diep@gmail.com", "0222333444");
+		admin.createEmployeecreateEmployee(Rule.Shipper, "022334411398", "Thai Nhu Huyen", new Date(2002, 3, 13), new Date(2027, 2, 9), "Female", "Long Xuyen", "huyen@gmail.com", "0333333345");
+		
 	}
-
 	public void initCustomer() {
 		center.register("Pham Huy Hoang", "0812838212", "huyhoang@gamil.com", "huyhoang");
 		center.register("To Thi Thao", "0942323123", "thaocute@gmail.com", "123thaocute");
@@ -35,15 +72,42 @@ public class MainController {
 		center.register("Cuu Van Hoa", "0874646473", "hoa@gmail.com", "73hduoicato");
 		center.register("Truong Y", "099983876", "yeuhoa@gmail.com", "yeuhoanhieulam");
 		center.register("Huong Ly Nguyet", "0356726374", "nguyetly@gmail.com", "hksa33");
-
+		center.register("Chau Van Loc", "0453344345", "imposter@gamil.com", "locimposter");
+		center.register("Hoang Diec Su", "0999877787", "hoangdaica@gmail.com", "hoangdenhatmynam");
+		center.register("Au Duong Phong", "03334244555", "phongchetchoc@gmail.com", "phongkinh");
+		center.register("Xuan Toc Do", "0977777777", "sodobangmom@gmail.com", "99999");
+		center.register("Chi Pheo", "0222343455", "daicaxomchua@gmail.com", "thino<3");
 	}
-
+	public void initProduct() {
+		ProductObserver burgerBeef = new BurgerBeef("Burger Bò", 10, 30000, "../FastFoodStoreManager/img/burgurBeef.png", center);
+		ProductObserver burgerChicken = new BurgerChicken("Burger Gà", 10, 25000, "../FastFoodStoreManager/img/burgurChicken.png", center);
+		ProductObserver burgerCheese = new BurgerCheese("Burger phô mai", 10, 25000, "../FastFoodStoreManager/img/burgurCheese.png", center);
+		ProductObserver spaghetti = new Spagetti("Mì Spaghetti", 10, 35000, "../FastFoodStoreManager/img/spaghetti.png", center);
+		ProductObserver friedChicken = new FriedChicken("Gà rán", 10, 30000, "../FastFoodStoreManager/img/friedChicken.png", center);
+		ProductObserver friedFrench = new FrenchFries("Khoai tây chiên", 10, 15000, "../FastFoodStoreManager/img/chips.png", center);
+		ProductObserver burgurShrimp = new BurgerShrimp("Burger Tôm", 10, 25000, "../FastFoodStoreManager/img/burgurShrimp.png", center);
+		ProductObserver burgerFish = new BurgerFish("Burger cá", 10, 20000, "../FastFoodStoreManager/img/burgurFish.png", center);
+		ProductObserver beef = new Beef("Bò", 10, 15000, "../FastFoodStoreManager/img/beef.png", center);
+		ProductObserver chicken = new Chicken("Gà", 10, 10000, "../FastFoodStoreManager/img/chicken.png", center);
+		ProductObserver shrimp = new Shrimp("Tôm", 10, 10000, "../FastFoodStoreManager/img/shrimp.png", center);
+		ProductObserver carbonateSoftDrink = new CarbonateSoftDrink("Nước giải khát có gas", 10, 10000, "../FastFoodStoreManager/img/carbonateSoftDrink.png", center);
+		ProductObserver juice = new Juice("Nước ép trái cây", 10, 15000, "../FastFoodStoreManager/img/juice.png", center);
+		ProductObserver mixed = new Mixed("Nước uông pha chế", 10, 15000, "../FastFoodStoreManager/img/mixed.png", center);
+		ProductObserver tea = new Tea("Trà", 10, 10000, "../FastFoodStoreManager/img/tea.png", center);
+		ProductObserver cheese = new Cheese("Kem cheese", 10, 5000, "../FastFoodStoreManager/img/kCheese", center);
+		ProductObserver pudding = new Pudding("Pudding", 10, 5000, "../FastFoodStoreManager/img/pudding.png", center);
+		ProductObserver sugar = new Sugar("Đường", 10, 2000, "../FastFoodStoreManager/img/sugar.png", center);
+	}
+	
 	public void init() {
 		center = Center.getInstance();
 		initCustomer();
+		initProduct();
 		PersonObserver ad1 = new Admin("ChauVanLoc", "0346128692", "locchau.220401@gmail.com", "chauvanloc", center);
+		PersonObserver ad2 = new Admin("Tran Tu Nguyet", "0914945114", "trantunguyet@gmai.com", "trantunguyet", center);
 		MainView mainView = new MainView(center);
 		initEmployee((Admin) ad1);
+		initEmployee((Admin) ad2);
 		mainView.display();
 	}
 
--- QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/views/SwingTableExample.java
+++ QuanLyCuaHangThucAnNhanh/FastFoodStoreManager/src/views/SwingTableExample.java
@@ -1,5 +1,7 @@
 package views;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -7,101 +9,128 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
 
 public class SwingTableExample extends JFrame {
 
-	public SwingTableExample(ArrayList<MyObject> dataList) {
-		setTitle("Swing Table Example");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public SwingTableExample(ArrayList<MyObject> dataList) {
+        setTitle("Swing Table Example");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// Tạo một custom table model với danh sách object và các tên cột
-		CustomTableModel tableModel = new CustomTableModel(dataList);
+        // Create a custom table model with the object list and column names
+        CustomTableModel tableModel = new CustomTableModel(dataList);
 
-		// Tạo JTable với custom table model
-		JTable table = new JTable(tableModel);
+        // Create JTable with the custom table model
+        JTable table = new JTable(tableModel);
+        
+        // Set custom cell renderer to apply hover effect
+        table.setDefaultRenderer(Object.class, new HoverCellRenderer());
 
-		// Thêm JTable vào JScrollPane để hỗ trợ cuộn
-		JScrollPane scrollPane = new JScrollPane(table);
-		getContentPane().add(scrollPane);
+        // Add JTable to JScrollPane for scrolling support
+        JScrollPane scrollPane = new JScrollPane(table);
+        getContentPane().add(scrollPane);
 
-		pack();
-		setLocationRelativeTo(null);
-	}
+        pack();
+        setLocationRelativeTo(null);
+    }
 
-	// Custom table model kế thừa từ AbstractTableModel
-	class CustomTableModel extends AbstractTableModel {
-		private ArrayList<MyObject> dataList;
-		private String[] columnNames = { "Column 1", "Column 2", "Column 3" };
+    // Custom table model extends AbstractTableModel
+    class CustomTableModel extends AbstractTableModel {
+        private ArrayList<MyObject> dataList;
+        private String[] columnNames = { "Column 1", "Column 2", "Column 3" };
 
-		public CustomTableModel(ArrayList<MyObject> dataList) {
-			this.dataList = dataList;
-		}
+        public CustomTableModel(ArrayList<MyObject> dataList) {
+            this.dataList = dataList;
+        }
 
-		@Override
-		public int getRowCount() {
-			return dataList.size();
-		}
+        @Override
+        public int getRowCount() {
+            return dataList.size();
+        }
 
-		@Override
-		public int getColumnCount() {
-			return columnNames.length;
-		}
+        @Override
+        public int getColumnCount() {
+            return columnNames.length;
+        }
 
-		@Override
-		public String getColumnName(int column) {
-			return columnNames[column];
-		}
+        @Override
+        public String getColumnName(int column) {
+            return columnNames[column];
+        }
 
-		@Override
-		public Object getValueAt(int row, int column) {
-			MyObject myObject = dataList.get(row);
-			switch (column) {
-			case 0:
-				return myObject.getProperty1();
-			case 1:
-				return myObject.getProperty2();
-			case 2:
-				return myObject.getProperty3();
-			default:
-				return null;
-			}
-		}
-	}
+        @Override
+        public Object getValueAt(int row, int column) {
+            MyObject myObject = dataList.get(row);
+            switch (column) {
+                case 0:
+                    return myObject.getProperty1();
+                case 1:
+                    return myObject.getProperty2();
+                case 2:
+                    return myObject.getProperty3();
+                default:
+                    return null;
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			ArrayList<MyObject> dataList = new ArrayList<>();
-			dataList.add(new MyObject("Value 1", "Value 2", "Value 3"));
-			dataList.add(new MyObject("Value 4", "Value 5", "Value 6"));
-			dataList.add(new MyObject("Value 7", "Value 8", "Value 9"));
+    // Custom cell renderer to apply hover effect
+    class HoverCellRenderer extends DefaultTableCellRenderer {
+        private Color hoverBackgroundColor = new Color(200, 200, 255);
 
-			SwingTableExample example = new SwingTableExample(dataList);
-			example.setVisible(true);
-		});
-	}
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                                                       boolean hasFocus, int row, int column) {
+            Component cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            // Apply hover effect when the cell is not selected
+            if (!isSelected) {
+                if (table.isRowSelected(row)) {
+                    cellComponent.setBackground(table.getSelectionBackground());
+                } else {
+                    cellComponent.setBackground(row % 2 == 0 ? table.getBackground() : hoverBackgroundColor);
+                }
+            }
+
+            return cellComponent;
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ArrayList<MyObject> dataList = new ArrayList<>();
+            dataList.add(new MyObject("Value 1", "Value 2", "Value 3"));
+            dataList.add(new MyObject("Value 4", "Value 5", "Value 6"));
+            dataList.add(new MyObject("Value 7", "Value 8", "Value 9"));
+            dataList.add(new MyObject("số 4", "fad", "mous"));
+
+            SwingTableExample example = new SwingTableExample(dataList);
+            example.setVisible(true);
+        });
+    }
 }
 
-// Định nghĩa lớp MyObject để biểu diễn đối tượng trong ArrayList
+// Define the MyObject class to represent objects in the ArrayList
 class MyObject {
-	private String property1;
-	private String property2;
-	private String property3;
+    private String property1;
+    private String property2;
+    private String property3;
 
-	public MyObject(String property1, String property2, String property3) {
-		this.property1 = property1;
-		this.property2 = property2;
-		this.property3 = property3;
-	}
+    public MyObject(String property1, String property2, String property3) {
+        this.property1 = property1;
+        this.property2 = property2;
+        this.property3 = property3;
+    }
 
-	public String getProperty1() {
-		return property1;
-	}
+    public String getProperty1() {
+        return property1;
+    }
 
-	public String getProperty2() {
-		return property2;
-	}
+    public String getProperty2() {
+        return property2;
+    }
 
-	public String getProperty3() {
-		return property3;
-	}
+    public String getProperty3() {
+        return property3;
+    }
 }
